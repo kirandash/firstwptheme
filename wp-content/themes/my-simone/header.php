82b7cc7463070a6a439fb6464491555165cef295
kiran kumar dash <thekirankumardash@gmail.com>
2015-09-27 13:49:30 +0530

    Add header image behind site branding with site-title and description section. Bug in customizer.js that doesn't display background image on disablinh header text.

--- wp-content/themes/my-simone/header.php
+++ wp-content/themes/my-simone/header.php
@@ -22,22 +22,28 @@
 <div id="page" class="hfeed site">
 
 	<header id="masthead" class="site-header" role="banner">
-		
-        <?php if ( get_header_image() ) : ?>
-        <div class="header-image">
-            <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-                <img src="<?php header_image(); ?>" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="">
-            </a>
-        </div>
-        <?php endif; // End header image check. ?>
     
+   		<?php if ( get_header_image() && ('blank' == get_header_textcolor()) ) { ?>
+            <figure class="header-image">
+                <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+                        <img src="<?php header_image(); ?>" width="< ?>" height="<?php echo get_custom_header()->height; ?>" alt="">
+                </a>
+            </figure>
+        <?php } // End header image check. ?>
     
-        <div class="site-branding">
+        <?php 
+			  if ( get_header_image() && !('blank' == get_header_textcolor()) ) { 
+				  echo '<div class="site-branding header-background-image" style="background-image: url(' . get_header_image() . ')">'; 
+			  } else {
+				  echo '<div class="site-branding">';
+			  }
+		  ?>
+
 			<div class="title-box">
-                <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-                <h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-            </div>
-		</div>
+            <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+            <h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+        </div>
+        </div>  
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
 			<h1 class="menu-toggle"><?php _e( 'Menu', 'my-simone' ); ?></h1>
