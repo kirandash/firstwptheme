01fd2565c4d53a0ffd79e277a47f8adf8ff3397f
kiran kumar dash <thekirankumardash@gmail.com>
2015-10-14 18:02:02 +0530

    START THE SINGLE POST TEMPLATE. Changing the single post template content structure.

--- wp-content/themes/my-simone/content-single.php
+++ wp-content/themes/my-simone/content-single.php
@@ -6,6 +6,16 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
+      
+        <?php
+            /* translators: used between list items, there is a space after the comma */
+            $category_list = get_the_category_list( __( ', ', 'my-simone' ) );
+        
+            if ( my_simone_categorized_blog() ) {
+                echo '<div class="category-list">' . $category_list . '</div>';
+            }
+        ?>
+        
 		<h1 class="entry-title"><?php the_title(); ?></h1>
 
 		<div class="entry-meta">
@@ -24,39 +34,11 @@
 	</div><!-- .entry-content -->
 
 	<footer class="entry-footer">
+		
 		<?php
-			/* translators: used between list items, there is a space after the comma */
-			$category_list = get_the_category_list( __( ', ', 'my-simone' ) );
-
-			/* translators: used between list items, there is a space after the comma */
-			$tag_list = get_the_tag_list( '', __( ', ', 'my-simone' ) );
-
-			if ( ! my_simone_categorized_blog() ) {
-				// This blog only has 1 category so we just need to worry about tags in the meta text
-				if ( '' != $tag_list ) {
-					$meta_text = __( 'This entry was tagged %2$s. Bookmark the <a href="%3$s" rel="bookmark">permalink</a>.', 'my-simone' );
-				} else {
-					$meta_text = __( 'Bookmark the <a href="%3$s" rel="bookmark">permalink</a>.', 'my-simone' );
-				}
-
-			} else {
-				// But this blog has loads of categories so we should probably display them here
-				if ( '' != $tag_list ) {
-					$meta_text = __( 'This entry was posted in %1$s and tagged %2$s. Bookmark the <a href="%3$s" rel="bookmark">permalink</a>.', 'my-simone' );
-				} else {
-					$meta_text = __( 'This entry was posted in %1$s. Bookmark the <a href="%3$s" rel="bookmark">permalink</a>.', 'my-simone' );
-				}
-
-			} // end check for categories on this blog
-
-			printf(
-				$meta_text,
-				$category_list,
-				$tag_list,
-				get_permalink()
-			);
-		?>
-
+            echo get_the_tag_list( '<ul><li><i class="fa fa-tag"></i>', '</li><li><i class="fa fa-tag"></i>', '</li></ul>' );
+        ?>
+        
 		<?php edit_post_link( __( 'Edit', 'my-simone' ), '<span class="edit-link">', '</span>' ); ?>
 	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
