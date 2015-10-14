2bb3e51fb71d4e259371db8c16410a373d2619e9
kiran kumar dash <thekirankumardash@gmail.com>
2015-10-14 16:56:35 +0530

    Add new widget

--- wp-content/themes/my-simone/functions.php
+++ wp-content/themes/my-simone/functions.php
@@ -79,6 +79,18 @@ function my_simone_widgets_init() {
 		'before_title'  => '<h1 class="widget-title">',
 		'after_title'   => '</h1>',
 	) );
+	
+	
+	register_sidebar( array(
+		'name'          => __( 'Footer Widgets', 'my-simone' ),
+		'description'   => __( 'Footer widgets area appears in the footer of the site.', 'my-simone' ),
+		'id'            => 'sidebar-2',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h1 class="widget-title">',
+		'after_title'   => '</h1>',
+	) );
+					
 }
 add_action( 'widgets_init', 'my_simone_widgets_init' );
 
