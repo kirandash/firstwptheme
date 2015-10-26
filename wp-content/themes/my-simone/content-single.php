fcbc016201e8fe70112651525830c41ef3d02109
kiran kumar dash <thekirankumardash@gmail.com>
2015-10-26 23:26:19 +0530

    Add featured images to templates

--- wp-content/themes/my-simone/content-single.php
+++ wp-content/themes/my-simone/content-single.php
@@ -5,7 +5,17 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
+	
+        
+    <?php 
+        if (has_post_thumbnail()) {
+            echo '<div class="single-post-thumbnail clear">';
+            echo the_post_thumbnail('large-thumb');
+            echo '</div>';
+        }
+    ?>
+                
+    <header class="entry-header">
       
         <?php
             /* translators: used between list items, there is a space after the comma */
