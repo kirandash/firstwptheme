1f77e137370b8b45b2de85c57ecc6aac5e79f124
kiran kumar dash <thekirankumardash@gmail.com>
2015-12-22 22:56:07 +0530

    Creating a custom display for the most recent post

--- wp-content/themes/my-simone/content.php
+++ wp-content/themes/my-simone/content.php
@@ -5,16 +5,33 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<div class="index-box">
-    <?php
-    if (has_post_thumbnail()) {
-        echo '<div class="small-index-thumbnail clear">';
-        echo '<a href="' . get_permalink() . '" title="' . __('Read ', 'my-simone') . get_the_title() . '" rel="bookmark">';
-        echo the_post_thumbnail('index-thumb');
-        echo '</a>';
-        echo '</div>';
-    }
-    ?>         
+	
+		<?php 
+        if( $wp_query->current_post == 0 && !is_paged() && is_front_page() ) { // Custom template for the first post on the front page
+            if (has_post_thumbnail()) {
+                echo '<div class="front-index-thumbnail clear">';
+                echo '<div class="image-shifter">';
+                echo '<a href="' . get_permalink() . '" title="' . __('Read ', 'my-simone') . get_the_title() . '" rel="bookmark">';
+                echo the_post_thumbnail('large-thumb');
+                echo '</a>';
+                echo '</div>';
+                echo '</div>';
+            } 
+            echo '<div class="index-box';
+            if (has_post_thumbnail()) { echo ' has-thumbnail'; };
+            echo '">';
+        } else {
+            echo '<div class="index-box">';
+            if (has_post_thumbnail()) {
+                echo '<div class="small-index-thumbnail clear">';
+                echo '<a href="' . get_permalink() . '" title="' . __('Read ', 'my-simone') . get_the_title() . '" rel="bookmark">';
+                echo the_post_thumbnail('index-thumb');
+                echo '</a>';
+                echo '</div>';
+            }
+        }
+        ?>
+                          
         <header class="entry-header">
                         
             <?php
@@ -42,14 +59,23 @@
         </header><!-- .entry-header -->
     
         
-        <div class="entry-content">
+        
+		<?php 
+        if( $wp_query->current_post == 0 && !is_paged() && is_front_page() ) { 
+            echo '<div class="entry-content">';
+            the_content( __( '', 'my-simone' ) );
+            echo '</div>';
+            echo '<footer class="entry-footer continue-reading">';
+            echo '<a href="' . get_permalink() . '" title="' . __('Read ', 'my-simone') . get_the_title() . '" rel="bookmark">Read the article<i class="fa fa-arrow-circle-o-right"></i></a>'; 
+            echo '</footer><!-- .entry-footer -->';
+        } else { ?>
+            <div class="entry-content">
             <?php the_excerpt(); ?>
-        </div><!-- .entry-content -->
-        
-    
-        
-        <footer class="entry-footer continue-reading">
+            </div><!-- .entry-content -->
+            <footer class="entry-footer continue-reading">
             <?php echo '<a href="' . get_permalink() . '" title="' . __('Continue Reading ', 'my-simone') . get_the_title() . '" rel="bookmark">Continue Reading<i class="fa fa-arrow-circle-o-right"></i></a>'; ?>
-        </footer><!-- .entry-footer -->
+            </footer><!-- .entry-footer -->
+        <?php } ?>
+                        
 	</div>
 </article><!-- #post-## -->
