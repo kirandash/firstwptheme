3e8078d7596ccd77f82b83bf978648b8a950f8fe
kiran kumar dash <thekirankumardash@gmail.com>
2015-12-23 22:41:44 +0530

    12.1 - Styling pages Chapter 12: Working with Static Pages START

--- wp-content/themes/my-simone/content-page.php
+++ wp-content/themes/my-simone/content-page.php
@@ -7,7 +7,18 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
+        
+    <?php 
+    if (has_post_thumbnail()) {
+        echo '<div class="single-post-thumbnail clear">';
+        echo '<div class="image-shifter">';
+        echo the_post_thumbnail('large-thumb');
+        echo '</div>';
+        echo '</div>';
+    }
+    ?>
+                    
+    <header class="entry-header">
 		<h1 class="entry-title"><?php the_title(); ?></h1>
 	</header><!-- .entry-header -->
 
